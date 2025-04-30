9f9184633e894de09728d6b8a367eefecfcf1ffc
vivek-dhamatpb <vivek.d@peerbits.com>
2025-04-30 13:07:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hello src/App.js</code> and save to reload.
+         Hello DevOps!!! Have a Good Day
         </p>
         <a
           className="App-link"
