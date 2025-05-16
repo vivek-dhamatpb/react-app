62841ea9bece5c87f055e9279361dbebd7581ee4
vivek-dhamatpb <vivek.d@peerbits.com>
2025-05-16 11:43:34 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello DevOps!!!! Have a Good Day 
+         Hello DevOps!!!! Have a Good weekend
         </p>
         <a
           className="App-link"
